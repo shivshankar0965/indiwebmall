62359c45537d162a15ab460a2e016316177c6bd4
Kausik Bhaumik <kausikbhaumik42@gmail.com>
2023-01-18 17:38:10 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Navbar from "./Components/Navbar/Navbar";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <div style={{height:'2000px',background:'teal'}}></div>
+    </div>
+  );
 }
 
 export default App;
