8bcaacf9398511404e28d1add0de2481e16cb8ee
Rishi <rishirajbhardwaj20@gmail.com>
2023-01-19 23:54:26 +0530

    Thursday Commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import GroceryRRB from "./Components/First_Column_rrb/Grocery-rrb";
 import JewelryRRB from "./Components/Second_Column_rrb/Jewelry1";
+import Slider from "./Components/First_Column_rrb/Slider";
 function App() {
   return (<div>
-    <Admin />
-      <Navbar />
-      <AllRoutes/>
-      <GroceryRRB/>
+      {/* <Admin /> */}
+      {/* <Navbar /> */}
+      {/* <AllRoutes/> */}
+      {/* <GroceryRRB/> */}
+      <Slider/>
       <JewelryRRB/>
       <div style={{height:'2000px',background:'teal'}}></div>
     </div>
