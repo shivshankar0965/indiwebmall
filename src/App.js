cbdc37d522e696c7fad0f468d43a53a2e6e9e79c
Shiv Shankar <shivshankar0965@gmail.com>
2023-01-19 21:04:06 +0530

    added pie chart

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import Signup from "./pages/Signup/Signup";
 import GroceryRRB from "./Components/First_Column_rrb/Grocery-rrb";
 import JewelryRRB from "./Components/Second_Column_rrb/Jewelry1";
 function App() {
-  return (<div>
-    <Admin />
-      <Navbar />
-      <AllRoutes/>
-      <GroceryRRB/>
-      <JewelryRRB/>
-      <div style={{height:'2000px',background:'teal'}}></div>
+  return (
+    <div>
+      {/* <Admin /> */}
+      {/* <Navbar /> */}
+      <AllRoutes />
+      {/* <GroceryRRB />
+      <JewelryRRB /> */}
+      {/* <div style={{ height: "2000px", background: "teal" }}></div> */}
     </div>
   );
 }
