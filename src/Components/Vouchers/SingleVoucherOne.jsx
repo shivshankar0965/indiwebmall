7ea1df5cd5d20909b39397b79875ca6db406ee7c
Deepak Kumar Fathkar <deepak.fathkar@gmail.com>
2023-01-22 01:22:55 +0530

    major modifications

--- src/Components/Vouchers/SingleVoucherOne.jsx
+++ src/Components/Vouchers/SingleVoucherOne.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import {
   Container,
@@ -8,7 +8,6 @@ import {
   Image,
   Text,
   GridItem,
-  Select,
   Box,
 } from "@chakra-ui/react";
 import { PSlider } from "../Slider/PSlider";
@@ -18,66 +17,27 @@ const SingleVoucherOne = () => {
   console.log("voucher_id:", voucher_id);
   // const navigate = useNavigate;
 
-  var sortedArr;
-
   const [data, setData] = useState([]);
-  // console.log("data:", data);
+  const [count, setCount] = useState({});
+  // console.log('count:', count.title)
+  // console.log('data:', data[0].image)
+
   const getData = async (voucher_id) => {
     let data = axios.get(`http://localhost:8080/vouchers/${voucher_id}`);
     let res = await data;
     // setData(res.data);
-    setData(res.data[voucher_id])
+    setCount(res.data);
+    setData(res.data[voucher_id]);
   };
   useEffect(() => {
     getData(voucher_id);
   }, [voucher_id]);
-  const handleAdd = (el) => {};
-
-  
-const selectHandler=(e)=>{
-  if(e.target.value=== "asc")
-  {
-    
-     sortedArr = data.sort((ele,el) =>{
-      // console.log('ele:', ele)
-      let x = Number(ele.price.slice(2,ele.price.length).split(",").join(""));
-      // console.log('x:', x)
-      let y = Number(el.price.slice(2,el.price.length).split(",").join(""));
-      // console.log('y:', y)
-      
-      if(y>x) return -1
-      if(x>y) return 1
-      return 0
-      
-    })
-    setData(sortedArr)
-    // console.log('sortedArr:', sortedArr)
-    
-  }
-  
-  
-  
-  // console.log(e.target.value)
-}
-// console.log('sortedArr:', sortedArr)
-useEffect(()=>{
-  setData(sortedArr)
-},[sortedArr])
 
   return (
     <Container maxW="full">
-      <Box align="right" >
-        <Box width={"sm"} >
-          <Select placeholder="Select option" onChange={selectHandler}>
-            <option value="asc">Ascending</option>
-            <option value="desc">Decending</option>
-          </Select>
-        </Box>
-      </Box>
-
       <Flex gap={5} py={5} pr={4} pl={4}>
         <Box
-          w="40%"
+          w={"40%"}
           h="50%"
           p={10}
           spacing={10}
@@ -85,7 +45,12 @@ useEffect(()=>{
           // bg={"grey"}
           boxShadow="base"
         >
-          <PSlider />
+          <PSlider
+            brand={count.title}
+            len={data.length}
+            img={count.image}
+            slogan={count.tagline}
+          />
         </Box>
         <Grid
           w="full"
@@ -93,28 +58,35 @@ useEffect(()=>{
           p={10}
           spacing={10}
           alignItems="flex-start"
-          templateColumns="repeat(4, 1fr)"
+          templateColumns={[
+            "repeat(1, 1fr)",
+            "repeat(2, 1fr)",
+            "repeat(3, 1fr)",
+            "repeat(4, 1fr)",
+          ]}
           gap={10}
           // bg={"red"}
           boxShadow="base"
         >
           {data?.map((el) => {
             return (
-              <GridItem
-                alignItems={"center"}
-                textAlign="left"
-                key={el.id}
-                onClick={() => handleAdd(el)}
-              >
+              <GridItem alignItems={"center"} textAlign="left" key={el.id}>
                 <Image src={el.image} alt={el.name} />
                 <Text>{el.name}</Text>
                 <Text fontWeight={"bold"}>{el.price}</Text>
-                <Text>{el.desc}</Text>
+                <Text color="#09ac63">{el.desc}</Text>
               </GridItem>
             );
           })}
         </Grid>
       </Flex>
+      <Box>
+        <Text boxShadow={"base"} p={2} color="gray.400">
+          Cashback would be added as Paytm Cash, which is One97 Communications
+          Ltd loyalty program. It can be used to pay for goods & services sold
+          by merchants that accept ‘Pay with Paytm’
+        </Text>
+      </Box>
     </Container>
   );
 };
--- src/pages/Vouchers.jsx
+++ src/pages/Vouchers.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import VoucherOne from "../Components/Vouchers/VoucherOne"
 import VoucherTwo from "../Components/Vouchers/VoucherTwo"
-// import VoucherTri from "../Components/Vouchers/VoucherTri"
+import VoucherTri from "../Components/Vouchers/VoucherTri"
 
 const Vouchers = () => {
   return (
     <div>
         <VoucherOne />
         <VoucherTwo />
-        {/* <VoucherTri /> */}
+        <VoucherTri />
     </div>
   )
 }
